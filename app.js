229c3e433e7ca971afcca35ba2fc33bb166a81f2
dedsecds22 <dedsecds@yahoo.com>
2026-02-15 00:04:44 +0200

    added technology module

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 // Load JSON data
 const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 const quiz = JSON.parse(fs.readFileSync('./quiz.json', 'utf8'));
+const technology = JSON.parse(fs.readFileSync('./technology.json', 'utf8'));
 
 // Set EJS as template engine
 app.set('view engine', 'ejs');
@@ -13,10 +14,11 @@ app.set('views', './views');
 // Serve static files
 app.use(express.static('public'));
 
-// Middleware to get language from query
+// Middleware to get language from query and set current path
 app.use((req, res, next) => {
   const lang = req.query.lang || 'en';
   res.locals.lang = lang;
+  res.locals.currentPath = req.path;
   next();
 });
 
@@ -53,6 +55,10 @@ app.get('/weapon/:id', (req, res) => {
 
 app.get('/quiz', (req, res) => {
   res.render('quiz', { quiz });
+});
+
+app.get('/technology', (req, res) => {
+  res.render('technology', { technology });
 });
 
 // Start server
